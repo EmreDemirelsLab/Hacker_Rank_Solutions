2ddd305a557308d6b79b42773656e3ff51052aa5
EmreDemirelsLab <h.emredemirel@gmail.com>
2025-03-24 19:22:47 +0300

    Hacker Rank

--- Find the Runner-Up Score!.py
+++ Find the Runner-Up Score!.py
@@ -13,24 +13,24 @@ if __name__ == '__main__':
     # Geriye kalan en yüksek puan, runner-up olacaktır
     print(max(unique_scores))
 
-
-Bu soruda, verilen bir puanlar listesinde runner-up yani ikinci en yüksek puanı bulmamız gerekiyor.
-Runner-up, en yüksek puandan sonra gelen en büyük puandır.
-
-Adım adım çözüm:
-Verilen Girdi:
-
-İlk satırda, n adlı bir tam sayı var. Bu, oyuncu sayısını belirtir. Ancak bu değeri kullanmamıza gerek yok,
-çünkü ikinci satırdaki listeyi doğrudan alabiliriz.
-
-İkinci satırda, n oyuncunun puanları bir liste olarak verilmiştir. Bu puanlar arasında aynı puanlar olabilir.
-
-Çözüm Planı:
-
-İlk olarak, verilen listeyi alıyoruz.
-
-Daha sonra, bu listeyi set (küme) yapısına dönüştürüyoruz. Çünkü küme, tekrarlanan değerleri otomatik olarak siler.
-
-Küme içindeki en büyük iki değeri buluyoruz. İlk olarak max() fonksiyonu ile en yüksek puanı, sonra bu maksimum puanı
-kümeden çıkararak ikinci en yüksek puanı buluyoruz.
-
+#
+# Bu soruda, verilen bir puanlar listesinde runner-up yani ikinci en yüksek puanı bulmamız gerekiyor.
+# Runner-up, en yüksek puandan sonra gelen en büyük puandır.
+#
+# Adım adım çözüm:
+# Verilen Girdi:
+#
+# İlk satırda, n adlı bir tam sayı var. Bu, oyuncu sayısını belirtir. Ancak bu değeri kullanmamıza gerek yok,
+# çünkü ikinci satırdaki listeyi doğrudan alabiliriz.
+#
+# İkinci satırda, n oyuncunun puanları bir liste olarak verilmiştir. Bu puanlar arasında aynı puanlar olabilir.
+#
+# Çözüm Planı:
+#
+# İlk olarak, verilen listeyi alıyoruz.
+#
+# Daha sonra, bu listeyi set (küme) yapısına dönüştürüyoruz. Çünkü küme, tekrarlanan değerleri otomatik olarak siler.
+#
+# Küme içindeki en büyük iki değeri buluyoruz. İlk olarak max() fonksiyonu ile en yüksek puanı, sonra bu maksimum puanı
+# kümeden çıkararak ikinci en yüksek puanı buluyoruz.
+#
